63976f5f0900636bcf5cfca6b30a9635c6a4ffa9
Lucifer <80453242+PANAKAR@users.noreply.github.com>
2023-08-05 01:07:20 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -14,10 +14,23 @@ import asset4 from './asset4.jpeg'
 import asset6 from './asset6.jpeg'
 import asset5 from './asset5.jpeg'
 import asset10 from './asset10.jpeg'
-
+import web from './web.svg'
+import management from './management.svg'
+import personalised_websites from './personalised_websites.svg'
+import starbucks from './company/starbucks.png'
+import tesla from './company/tesla.png'
+import shopify from './company/shopify.png'
+import meta from './company/meta.png'
 
 export {
   Logo,
+  shopify,
+  meta,
+  personalised_websites,
+  starbucks,
+  tesla,
+  web,
+  management,
   asset22,
   asset6,
   asset4,
