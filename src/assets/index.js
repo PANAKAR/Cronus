7bc15d2031a2f2374cb3c0961d74cc0dc627d0f0
Lucifer <80453242+PANAKAR@users.noreply.github.com>
2023-08-04 23:57:11 +0530

    added index

---
+++ src/assets/index.js
@@ -0,0 +1,96 @@
+import logo from "./logo.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import backend from '../assets/backend.png'
+import web from '../assets/web.svg'
+import management from '../assets/management.svg'
+import ad from '../assets/ad.svg'
+import personalised_websites from '../assets/personalised_websites.svg'
+
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import Logo from "./logo.jpg"
+import hero from "./asset9-removebg.png"
+import asset13 from './asset13.jpeg'
+import asset23 from './asset23.jpeg'
+import asset3 from './asset3.jpeg'
+import asset15 from './asset15.jpeg'
+import asset16 from './asset16.jpeg'
+import asset17 from './asset17.jpeg'
+import asset18 from './asset18.jpeg'
+import asset25 from './asset25.jpeg'
+import asset20 from './asset20.jpeg'
+import asset22 from './asset22.jpeg'
+import asset4 from './asset4.jpeg'
+import asset6 from './asset6.jpeg'
+import asset5 from './asset5.jpeg'
+import asset10 from './asset10.jpeg'
+
+
+export {
+  Logo,
+  asset22,
+  asset6,
+  asset4,
+  asset5,
+  asset10,
+  logo,
+  asset3,
+  asset15,
+  asset16,
+  asset17,
+  asset18,
+  asset20,
+  asset25,
+  asset23,
+  asset13,
+  hero,
+  backend,
+  creator,
+  mobile,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  web,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  management,
+  ad,
+  personalised_websites,
+};
